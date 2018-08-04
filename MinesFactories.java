fee0701b67499d74d1d6f7b304962784394a4ab9
Rubin <rubinkazan@users.noreply.github.com>
2018-08-04 11:12:53 +0200

    Update MinesFactories.java

--- MinesFactories.java
+++ MinesFactories.java
@@ -2,12 +2,15 @@ public class MinesFactories{
 
 int mineID = 0;
 int factID = 0;
+int x_coord = 0;
+int y_coord = 0;
 
-public MinesFactories(int m, int f){
+public MinesFactories(int m, int f, int x, int y){
+	y = y_coord;
+	x = x_coord;
 	m = mineID;
 	f = factID;
 }
-
 
 public int getMineID(int mineID()){
 	return mineID;
@@ -16,4 +19,15 @@ public int getMineID(int mineID()){
 public int getFactID(int factID()){
 	return factID;
 }
+
+public int getX(int x){
+	return x;
 }
+
+public int getY(int y){
+	return y;
+}
+
+
+}
+//x//y
