72fd1aeb4b08aecd7ed5007a9a1bb97d8be3409f
Rubin <rubinkazan@users.noreply.github.com>
2018-08-04 10:08:31 +0200

    Add files via upload

---
+++ blah.java
@@ -0,0 +1,28 @@
+public class Workers{
+private static final int min_cap = 1;
+private static final int exc_cap = 3;
+private static final int hau_cap = 5;
+public int uni_id = 0;
+public int workerNum
+
+public Workers(int workerNum,int uni_id,int workerType){
+	this.workerNum = workerNum;
+	this.uni_id = uni_id;
+}
+
+
+public void setMiner(int min_cap){
+	this.min_cap = min_cap;
+}
+
+public void setExcav(int exc_cap){
+	this.exc_cap = exc_cap;
+}
+
+public void  setHauler(int hau_cap){
+	this.hau_cap = hau_cap;
+}
+
+
+
+}
