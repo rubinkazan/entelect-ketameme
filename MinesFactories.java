06824148b3236274b49b66919dc9e666521df1c9
Rubin <rubinkazan@users.noreply.github.com>
2018-08-04 11:04:48 +0200

    Add files via upload

---
+++ MinesFactories.java
@@ -0,0 +1,19 @@
+public class MinesFactories{
+
+int mineID = 0;
+int factID = 0;
+
+public MinesFactories(int m, int f){
+	m = mineID;
+	f = factID;
+}
+
+
+public int getMineID(int mineID()){
+	return mineID;
+}
+
+public int getFactID(int factID()){
+	return factID;
+}
+}
